3eb7d5d1690d132d8b57cf67ff545d485df9c521
idhpaul <idhpaul@naver.com>
2024-09-04 19:35:22 +0900

    wip(app): gcloud tts unify demo done

--- TTSUnify/Pages/Index.cshtml.cs
+++ TTSUnify/Pages/Index.cshtml.cs
@@ -17,7 +17,6 @@ namespace TTSUnify.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-        private readonly TextToSpeechClient _textToSpeechClient;
 
         [BindProperty]
         public List<UserInputModel> UserInputs { get; set; } = default!;
@@ -28,50 +27,31 @@ namespace TTSUnify.Pages
             Debug.WriteLine("IndexModel");
         }
 
-        private void DeleteAllAudioFiles()
-        {
-            var _audioDirectory = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "audio");
-
-            if (Directory.Exists(_audioDirectory))
-            {
-                var audioFiles = Directory.GetFiles(_audioDirectory, "*.mp3");
-
-                foreach (var file in audioFiles)
-                {
-                    try
-                    {
-                        System.IO.File.Delete(file);
-                    }
-                    catch (IOException ex)
-                    {
-                        // 파일 삭제 중 예외가 발생하면 처리 (예: 파일이 열려 있는 경우)
-                        // 로그를 남기거나, 사용자에게 알릴 수 있습니다.
-                        // 예외를 무시하고 계속 삭제를 시도할 수도 있습니다.
-                        Console.WriteLine($"Error deleting file {file}: {ex.Message}");
-                    }
-                }
-            }
-        }
 
         public void OnGet()
         {
         }
 
 
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostTransformAsync()
         {
-
-
-            // 오디오 파일 삭제
-            DeleteAllAudioFiles();
 
             var audioUrls = new List<string>();
             var audioDirectory = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "audio");
+            var timeFolder = DateTime.Now.ToString("MMdd_HH-mm-ss");
+            var timeFolderDirectory = Path.Combine(audioDirectory, timeFolder);
             var client = TextToSpeechClient.Create();
+            var audioFileIndex = 1;
 
-            if (!Directory.Exists(audioDirectory))
+            // UserInputs가 null이거나 비어있으면 빠져나옴
+            if (UserInputs == null || UserInputs.Count == 0 || UserInputs[0].Text == null)
             {
-                Directory.CreateDirectory(audioDirectory);
+                return Page(); // 페이지를 다시 렌더링하거나 다른 응답을 반환
+            }
+
+            if (!Directory.Exists(timeFolderDirectory))
+            {
+                Directory.CreateDirectory(timeFolderDirectory);
             }
 
             foreach (var inputModel in UserInputs)
@@ -81,10 +61,9 @@ namespace TTSUnify.Pages
                     Text = inputModel.Text
                 };
 
-                // 성별에 따른 음성 설정
                 var voice = new VoiceSelectionParams
                 {
-                    LanguageCode = "ko-KR",  // 한국어 음성
+                    LanguageCode = "ko-KR",
                     Name = inputModel.Gender == "Female" ? "ko-KR-Standard-B" : "ko-KR-Standard-C",
                     SsmlGender = inputModel.Gender == "Female" ? SsmlVoiceGender.Female : SsmlVoiceGender.Male
                 };
@@ -96,23 +75,22 @@ namespace TTSUnify.Pages
 
                 var response = await client.SynthesizeSpeechAsync(input, voice, audioConfig);
 
-                var fileName = $"{Guid.NewGuid()}.mp3";
-                var filePath = Path.Combine(audioDirectory, fileName);
+                var fileName = $"{audioFileIndex++}_{Guid.NewGuid()}.mp3";
+                var filePath = Path.Combine(timeFolderDirectory, fileName);
                 await System.IO.File.WriteAllBytesAsync(filePath, response.AudioContent.ToByteArray());
 
-                audioUrls.Add($"/audio/{fileName}");
+                audioUrls.Add($"/audio/{timeFolder}/{fileName}");
             }
 
+            audioUrls.Add(AudioCombine(timeFolderDirectory, timeFolder));
+
             return new JsonResult(audioUrls);
+
         }
 
-        public void OnGetFoo()
+        public IActionResult OnPostCombine([FromBody] List<string> filePaths)
         {
-            Debug.WriteLine("get foo");
-        }
 
-        public IActionResult OnPostCombine()
-        {
             var audioDirectory = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "audio");
             var audioFiles = Directory.GetFiles(audioDirectory, "*.mp3").OrderBy(file => file).ToList();
             var combinedFilePath = Path.Combine(audioDirectory, "combined.mp3");
@@ -141,6 +119,37 @@ namespace TTSUnify.Pages
 
             // 최종 MP3 파일의 URL 반환
             return new JsonResult(new { combinedFileUrl = "/audio/combined.mp3" });
+        }
+
+        private string AudioCombine(string timeFolderDirectory, string timeFolder)
+        {
+            // 타겟 샘플레이트 및 채널 설정
+            int targetSampleRate = 24000;
+            int targetChannels = 1;
+            int silenceDurationInMs = 800; // 0.8초
+            var combinedFileName = "combined.mp3";
+
+            var audioFiles = Directory.GetFiles(timeFolderDirectory, "*.mp3").OrderBy(file => file).ToList();
+            var combinedFilePath = Path.Combine(timeFolderDirectory, combinedFileName);
+
+            // MP3 파일 병합을 위한 출력 스트림 설정
+            using (var outputStream = new FileStream(combinedFilePath, FileMode.Create))
+            using (var mp3Writer = new LameMP3FileWriter(outputStream, new WaveFormat(targetSampleRate, targetChannels), LAMEPreset.VBR_90))
+            {
+                foreach (var file in audioFiles)
+                {
+                    // MP3 파일 복사
+                    using (var mp3Reader = new Mp3FileReader(file))
+                    {
+                        mp3Reader.CopyTo(mp3Writer);
+                    }
+
+                    // 무음 삽입
+                    InsertSilence(mp3Writer, targetSampleRate, targetChannels, silenceDurationInMs);
+                }
+            }
+
+            return $"/audio/{timeFolder}/{combinedFileName}";
         }
 
         private void InsertSilence(LameMP3FileWriter writer, int sampleRate, int channels, int durationInMs)
